4795469775b29bc165b6e70d328c6d14d697bf38
Alexander Mulyk <57097478+AlexBringston@users.noreply.github.com>
2022-05-27 13:42:31 +0300

    Fixed workers creation place
    Fixed executor so the workers are created only once in constructor, and not in methods.

--- src/main/java/lab2/CustomExecutor.java
+++ src/main/java/lab2/CustomExecutor.java
@@ -6,19 +6,20 @@ import java.util.function.Function;
 
 public class CustomExecutor {
 
-    private int maxWorkers;
-
-    private Queue<WorkItem> queue = new LinkedBlockingQueue();
+    private Queue<WorkItem> queue = new LinkedBlockingQueue<>();
     private WorkerThread[] workerThreads;
 
     public CustomExecutor(int maxWorkers) {
-        this.maxWorkers = maxWorkers;
+        if (maxWorkers <= 0) throw new IllegalArgumentException("You need to enter a number bigger than 0!");
+        workerThreads = new WorkerThread[maxWorkers];
+        for (int i = 0; i < workerThreads.length; i++) {
+            workerThreads[i] = new WorkerThread();
+        }
     }
 
     public FutureResult execute(Function function, int arg) {
         WorkItem<Integer, Integer> workItem = new WorkItem<>(function, arg);
         queue.add(workItem);
-        workerThreads = new WorkerThread[]{new WorkerThread()};
         workerThreads[0].setQueue(queue);
         workerThreads[0].start();
         return workItem.getFutureResult();
@@ -27,17 +28,13 @@ public class CustomExecutor {
     public FutureResult[] map(Function function, int[] args) {
         WorkItem<Integer, Integer>[] workItems = new WorkItem[args.length];
         FutureResult[] results = new FutureResult[args.length];
-        workerThreads = new WorkerThread[maxWorkers];
 
         for (int i = 0; i < args.length; i++) {
             workItems[i] = new WorkItem<>(function, args[i]);
             queue.add(workItems[i]);
         }
-        for (int i = 0; i < workerThreads.length; i++) {
-            workerThreads[i] = new WorkerThread();
-            workerThreads[i].setQueue(queue);
-        }
         for (WorkerThread workerThread : workerThreads) {
+            workerThread.setQueue(queue);
             workerThread.start();
         }
         for (int i = 0; i < args.length; i++) {
